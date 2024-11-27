32740c31c91258a613d4a9d5d35c4fd3d8d8f581
Abdul Haseeb Dar <haseebdar696@gmail.com>
2024-11-28 03:11:52 +0500

    new changes

--- server.js
+++ server.js
@@ -165,6 +165,7 @@ app.get('/api/tenant', async (req, res) => {
 
 app.post('/api/property', upload.fields([{ name: 'photo1', maxCount: 1 }, { name: 'photo2', maxCount: 1 }]), async (req, res) => {
     const { owner, contact, address, rentPrice, roomSize, features } = req.body;
+    console.log(owner, contact, address, rentPrice, roomSize, features, req.files);
     const property = new Property({
         owner, 
         contact, 
